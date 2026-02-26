167c88a80a64613b6e22b4217621f231c2e9c960
Jane Batakariza <janebatakariza@gmail.com>
2026-02-26 21:01:04 +0200

    fixed routes

--- route/studentroutes.js
+++ route/studentroutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const student = require('./model/models.js');
 const route= express.Router();
+
 
 route.post("/",(req,res)=>{
     try{
+const {name,age,level}= req.body;
+const newStudent = await student.create
 
     }
+    catch{
+
+        console.error({error:  message.error
+        });
+    }
 })
+module.exports = route;
